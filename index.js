b638c0d26cb1bc8e65e3e1990dc3ad1056f4ac40
maimuna003 <135684307+maimuna003@users.noreply.github.com>
2023-06-18 19:07:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function scuberGreetingForFeet(){
+  function scuberGreetingForFeet(ride) {
   // Write your code here!
+  if (ride <= 400) {
+    return `This one is on me!`;
+  } else if (ride >= 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else if (ride >= 2500) {
+    return "No";
+  }
 }
 
 function ternaryCheckCity(){
+function ternaryCheckCity(city) {  
   // Write your code here!
+  if (city === "NYC") {
+    return `Ok, sounds good.`;
+  } else if (city !== "NYC") {
+    return "No go.";
+  }
+}
+  
 }
 
 function switchOnCharmFromTip(){
+  function switchOnCharmFromTip(tip) {
   // Write your code here!
+   if (tip === "generous") {
+    return `Thank you so much.`;
+  } else if (tip !== "generous") {
+    return `Thank you.`;
+  } else if ((tip = "anything else")) {
+    return `Bye`;
+  } 
 }
